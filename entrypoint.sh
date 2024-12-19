d5b1f956caa05113cd685c727fd1614138a5cefd
Jean Cruypenynck <filaton@me.com>
2024-12-19 13:55:03 +0100

    Add step 0

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@ step=$1
 if [ "$step" -gt 1 ]; then
     diff -d --minimal test_dummy_$((step - 1)).py test_dummy_$step.py -U 1000  | batcat --color=always
 else
-    batcat --color=always test_dummy_1.py
+    batcat --color=always test_dummy_$step.py
 fi
 
-set -x
-pytest --setup-plan test_dummy_$step.py
+if [ "$step" -gt 0 ]; then
+    set -x
+    pytest --setup-plan test_dummy_$step.py
+fi
---
+++ tests/test_dummy_0.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.mark.parametrized("color", ["red", "blue"])
+def test_dummy(color):
+    ...
